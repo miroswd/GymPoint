9f551212c9e12b144bb06b159561b9ef80e6f921
miroswd <altamirsantos93@hotmail.com>
2019-12-11 22:06:28 -0300

    Registration

---
+++ src/database/migrations/20191212003812-student-plan.js
@@ -0,0 +1,57 @@
+// Devo referenciar o aluno ao plano - gestão de matrícula
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('registrations', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'students',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'plans',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      start_date: {
+        // Início da matrícula - escolhida pelo o usuário
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        // Calculado automaticamente, baseado na duração
+        type: Sequelize.DATE,
+        // allowNull: false,
+      },
+      price: {
+        // Calculado automaticamente (price*duration)
+        type: Sequelize.DECIMAL(10, 2),
+        // allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('registrations');
+  },
+};

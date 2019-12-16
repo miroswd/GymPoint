f997269d535efda02f5f6a9a6add0682f0933cf0
miroswd <altamirsantos93@hotmail.com>
2019-12-16 19:12:56 -0300

    Register

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+// Credenciais do nodemailer
+
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e7d5cd70baf4c9',
+    pass: 'ec9258cdc6e775',
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gympoint.com>',
+  },
+};
